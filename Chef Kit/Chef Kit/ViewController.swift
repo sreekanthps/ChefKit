fae5d78e5e3459792a1352279b7dfd390be0b0ab
sreekanthps <sreekanth.pulicherla@gmail.com>
2020-10-14 21:03:52 +0800

    Initial project commit
    Adding the initial chef kit project

---
+++ Chef Kit/Chef Kit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chef Kit
+//
+//  Created by Swetha Sreekanth on 14/10/20.
+//  Copyright © 2020 Brewers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Chef Kit/Chef KitTests/Chef_KitTests.swift
@@ -0,0 +1,34 @@
+//
+//  Chef_KitTests.swift
+//  Chef KitTests
+//
+//  Created by Swetha Sreekanth on 14/10/20.
+//  Copyright © 2020 Brewers. All rights reserved.
+//
+
+import XCTest
+@testable import Chef_Kit
+
+class Chef_KitTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
